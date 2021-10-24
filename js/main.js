59c64739c791eb2bca521a54394a2b9eefaf1776
mayaemerson <emersonmaia@me.com>
2021-10-24 15:46:59 -0300

    consumindo api github

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
+
 const LinksSocialMedia = {
-  github = 'mayaemerson',
-  facebook = 'emerson.maia.186',
-  instagram = 'emersonmaiadp',
-  twitter = 'maiaemersondev'
+  github: 'mayaemerson',
+  facebook: 'emerson.maia.186',
+  instagram: 'emersonmaiadp',
+  twitter: 'maiaemersondev'
 }
 
-function ChargeSocialMedia(){
-  for(let li of sociallinks.children){
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
-    li.children[0].href =`https://${social}.com/${LinksSocialMedia[social]}`;
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
   }
 }
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    UserImage.src = data.avatar_url
+    userLogin.textContent = data.login
+  })
+}
+
+getGitHubProfileInfos()
